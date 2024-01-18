6d67adcc66d7ba65282728eaf7aa677ecf8bfc42
Kirill <kirill.nikolaev@everon.fi>
2024-01-18 16:33:48 +0200

    task 6 completed

--- exercise_1/integersReader.py
+++ exercise_1/integersReader.py
@@ -6,14 +6,18 @@
 def main():
     negative = 0
     even = 0
+    sum_divisible_by_three = 0
     number = int(input("Enter a number: "))
     while number != 0:
         if number < 0:
             negative += 1
         if number % 2 == 0:
             even += 1
+        if number > 0 and number % 3 == 0:
+            sum_divisible_by_three += number
         number = int(input("Enter a number: "))
     print("Number of negative integers:", negative)
     print("Number of even integers:", even)
+    print("Sum of positive integers divisible by three:", sum_divisible_by_three)
 
 main()
