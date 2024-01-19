2d2d503189c59577b9698351493438f5e5e95537
Kirill <kirill.nikolaev@everon.fi>
2024-01-19 09:39:28 +0200

    extract code from main into dedicated functions

--- exercise_1/arithmeticProgression.py
+++ exercise_1/arithmeticProgression.py
@@ -11,8 +11,13 @@ def print_progression(max_value):
         progression = [str(i) for i in range(3, max_value + 1, 3)]
         print("Procession is: " + ", ".join(progression))
 
-def main():
-    max_value = int(input("Give maximum value: "))
+def print_to_console(max_value, sum, sum_squares, count):
+    print_progression(max_value)
+    print("Number of terms:", count)
+    print("Sum of terms is:", sum)
+    print("Sum of squared terms is:", sum_squares)
+
+def build_progression(max_value):
     sum = 0
     sum_squares = 0
     count = 0
@@ -20,9 +25,11 @@ def main():
         sum += i
         sum_squares += i ** 2
         count += 1
-    print_progression(max_value)
-    print("Number of terms:", count)
-    print("Sum of terms is:", sum)
-    print("Sum of squared terms is:", sum_squares)
+    return sum, sum_squares, count
+
+def main():
+    max_value = int(input("Give maximum value: "))
+    sum, sum_squares, count = build_progression(max_value)
+    print_to_console(max_value, sum, sum_squares, count)
 
 main()
