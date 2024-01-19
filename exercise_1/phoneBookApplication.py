5126e81a5752b6d0b86f8b9573315f302a5d76ce
Kirill <kirill.nikolaev@everon.fi>
2024-01-19 10:14:06 +0200

    refactor main method

--- exercise_1/phoneBookApplication.py
+++ exercise_1/phoneBookApplication.py
@@ -2,22 +2,35 @@
 # Author: Kirill
 # Description: user can add and search phone numbers using the console
 
-def main():
-    phone_book = {}
+def get_number(phone_book):
+    name = input("name: ")
+    print(phone_book.get(name, "no number"))
+
+def add_number(phone_book):
+    name = input("name: ")
+    number = input("number: ")
+    phone_book[name] = number
+    print("ok!")
+
+def quit():
+    print("quitting...")
+    return True
+
+def work_with_phone_book(phone_book):
     while True:
         command = int(input("command (1 search, 2 add, 3 quit): "))
         if command == 1:
-            name = input("name: ")
-            print(phone_book.get(name, "no number"))
+            get_number(phone_book)
         elif command == 2:
-            name = input("name: ")
-            number = input("number: ")
-            phone_book[name] = number
-            print("ok!")
+            add_number(phone_book)
         elif command == 3:
-            print("quitting...")
-            break
+            if quit():
+                break
         else:
             print("Invalid command. Please enter 1, 2, or 3.")
 
+def main():
+    phone_book = {}
+    work_with_phone_book(phone_book)
+    
 main()
