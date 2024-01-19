a6a195c057a7043bf5bda5207a5e1a6430165c4d
Kirill <kirill.nikolaev@everon.fi>
2024-01-19 10:20:50 +0200

    refactor work with phone book function

--- exercise_1/phoneBookApplication.py
+++ exercise_1/phoneBookApplication.py
@@ -16,21 +16,21 @@ def quit():
     print("quitting...")
     return True
 
+def invalid_command():
+    print("Invalid command. Please enter 1, 2, or 3.")
+
 def work_with_phone_book(phone_book):
+    commands = {1: get_number, 2: add_number, 3: quit, 'default': invalid_command}
     while True:
         command = int(input("command (1 search, 2 add, 3 quit): "))
-        if command == 1:
-            get_number(phone_book)
-        elif command == 2:
-            add_number(phone_book)
-        elif command == 3:
-            if quit():
-                break
+        action = commands.get(command, commands['default'])
+        if action == quit and action():
+            break
         else:
-            print("Invalid command. Please enter 1, 2, or 3.")
+            action(phone_book)
 
 def main():
     phone_book = {}
     work_with_phone_book(phone_book)
-    
+
 main()
