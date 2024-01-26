d71ab30c231976a3c020d22cf70121ec4fe955fd
Kirill <kirill.nikolaev@everon.fi>
2024-01-26 17:17:27 +0200

    add terms description

---
+++ exercise_2/terms.py
@@ -0,0 +1,33 @@
+# File name: hello.py
+# Author: Kirill
+# Description: describe the terms below
+
+# a. Pseudocode: 
+# Pseudocode is a simplified, informal high-level description of a computer program or algorithm. 
+# It is written in a way that mimics the structure of a programming language 
+# but is intended for human reading rather than machine reading. 
+# It's often used for planning and explaining algorithms.
+
+# b. Algorithm: 
+# An algorithm is a step-by-step procedure to solve a particular problem. 
+# In the context of programming, 
+# an algorithm can be understood as a sequence of instructions 
+# that a software program will follow to complete a specific task.
+
+# c. Data Attribute: 
+# In object-oriented programming, a data attribute 
+# (also known as a field or property) is a variable 
+# that stores some data associated with an object. 
+# Each instance of a class has its own copy of the data attributes defined in the class.
+
+# d. Method: 
+# A method is a function that is associated with an object. 
+# In object-oriented programming, 
+# methods define the behavior of an object and can access 
+# and modify the data attributes of the object they belong to.
+
+# e. Constructor: 
+# A constructor is a special type of method in object-oriented programming 
+# that is automatically called when an object is created. 
+# It's typically used to initialize the data attributes of the new object. 
+# The name of the constructor method is usually the same as the name of the class.
