14fd517bd7aec337baa56cb6db7bc7157e71ef2e
Kirill <kirill.nikolaev@everon.fi>
2024-01-26 17:57:53 +0200

    add alarm which makes digits red when on

--- exercise_2/alarmClock.py
+++ exercise_2/alarmClock.py
@@ -9,11 +9,19 @@ class Clock:
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
+        self.alarm_hours = None
+        self.alarm_minutes = None
+        self.alarm_seconds = None
 
     def set_time(self, hours, minutes, seconds):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
+
+    def set_alarm(self, hours, minutes, seconds):
+        self.alarm_hours = hours
+        self.alarm_minutes = minutes
+        self.alarm_seconds = seconds
 
     def tick(self):
         if self.seconds == 59:
@@ -30,10 +38,17 @@ class Clock:
             self.seconds += 1
 
     def display_time(self):
-        return f"{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
+        if (self.alarm_hours is not None and self.hours >= self.alarm_hours and
+            self.alarm_minutes is not None and self.minutes >= self.alarm_minutes and
+            self.alarm_seconds is not None and self.seconds >= self.alarm_seconds):
+            return f"\033[91m{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}\033[0m"
+        else:
+            return f"{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
+
 
 
 my_clock = Clock(10, 30, 45)
+my_clock.set_alarm(10, 31, 0)
 
 while True:
     print(my_clock.display_time(), end='\r')
