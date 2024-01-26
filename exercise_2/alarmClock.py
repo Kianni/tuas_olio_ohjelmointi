b521e69bca5f32e5c7c7fbf4695147df81a648a5
Kirill <kirill.nikolaev@everon.fi>
2024-01-26 18:06:09 +0200

    add alarm on property

--- exercise_2/alarmClock.py
+++ exercise_2/alarmClock.py
@@ -12,6 +12,7 @@ class Clock:
         self.alarm_hours = None
         self.alarm_minutes = None
         self.alarm_seconds = None
+        self.alarm_on = False
 
     def set_time(self, hours, minutes, seconds):
         self.hours = hours
@@ -36,19 +37,23 @@ class Clock:
                 self.minutes += 1
         else:
             self.seconds += 1
-
-    def display_time(self):
+    
+            # Check if alarm should be on
         if (self.alarm_hours is not None and self.hours >= self.alarm_hours and
             self.alarm_minutes is not None and self.minutes >= self.alarm_minutes and
             self.alarm_seconds is not None and self.seconds >= self.alarm_seconds):
-            return f"\033[91m{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}\033[0m"
-        else:
-            return f"{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
+            self.alarm_on = True
+
+    def display_time(self):
+        time_string = f"{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}"
+        if self.alarm_on:
+            time_string = f"\033[91m{str(self.hours).zfill(2)}:{str(self.minutes).zfill(2)}:{str(self.seconds).zfill(2)}\033[0m"
+        return time_string
 
 
 
 my_clock = Clock(10, 30, 45)
-my_clock.set_alarm(10, 31, 0)
+my_clock.set_alarm(10, 30, 50)
 
 while True:
     print(my_clock.display_time(), end='\r')
