1333c35b201d2e161166c9889521687840b457fb
Kirill <kirill.nikolaev@everon.fi>
2024-02-01 18:46:05 +0200

    part 3 accomplished

--- exercise_3/lunchCard.py
+++ exercise_3/lunchCard.py
@@ -17,16 +17,29 @@ class LunchCard:
             print("Insufficient balance for luxurious lunch.")
         else:
             self.balance -= 5.90
+
+    def deposit_money(self, amount):
+        if amount < 0:
+            raise ValueError("Cannot deposit a negative amount.")
+        self.balance += amount
     
     #  string containing the balance: "The balance is X euros" 
     def __str__(self):
         return "The balance is {:.1f} euros".format(self.balance)
     
 def main():
-    card=LunchCard(6)
+    card=LunchCard(10)
+    print(card)
+    card.deposit_money(15)
+    print(card)
+    card.deposit_money(10)
+    print(card)
+    card.deposit_money(200)
     print(card)
     card.eat_ordinarly()
     print(card)
+
+    card.deposit_money(-10)
     card.eat_luxuriously()
     card.eat_ordinarly()
     print(card)
