589e878101a6f3e26813c961dba593346164b614
Kirill <kirill.nikolaev@everon.fi>
2024-02-01 18:40:03 +0200

    part 2 accomplished

--- exercise_3/lunchCard.py
+++ exercise_3/lunchCard.py
@@ -5,14 +5,30 @@
 class LunchCard:
     def __init__(self, balance:float):
         self.balance =balance
+
+    def eat_ordinarly(self):
+        if self.balance - 2.95 < 0:
+            print("Insufficient balance for ordinary lunch.")
+        else:
+            self.balance -= 2.95
     
-    #  string containing the balance: "The balance is X euros". 
-    # The available balance should be printed out with one decimal place precision. 
+    def eat_luxuriously(self):
+        if self.balance - 5.90 < 0:
+            print("Insufficient balance for luxurious lunch.")
+        else:
+            self.balance -= 5.90
+    
+    #  string containing the balance: "The balance is X euros" 
     def __str__(self):
         return "The balance is {:.1f} euros".format(self.balance)
     
 def main():
-    card = LunchCard(50.0)
+    card=LunchCard(6)
+    print(card)
+    card.eat_ordinarly()
+    print(card)
+    card.eat_luxuriously()
+    card.eat_ordinarly()
     print(card)
 
 if __name__ == "__main__":
