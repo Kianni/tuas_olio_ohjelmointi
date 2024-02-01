208dd96cbeb615b85c87a88abe4ff3e0ddf00a32
Kirill <kirill.nikolaev@everon.fi>
2024-02-01 18:03:35 +0200

    part 2 accomplished

--- exercise_3/statisticsOnNumbers.py
+++ exercise_3/statisticsOnNumbers.py
@@ -1,4 +1,4 @@
-# File: statisticsOnNumbers_1.py
+# File: statisticsOnNumbers.py
 # Author: Kirill Nikolaev
 # Description: class NumberStats has add_number() and count_numbers() methods
 
@@ -12,6 +12,16 @@ class NumberStats:
     def count_numbers(self):
         return len(self.__numbers)
     
+    def get_sum(self):
+        if self.count_numbers() == 0:
+            return 0
+        return sum(self.__numbers)
+    
+    def average(self):
+        if self.count_numbers() == 0:
+            return 0
+        return self.get_sum() / self.count_numbers()
+    
 def main():
     stats = NumberStats()
     stats.add_number(3)
@@ -19,5 +29,7 @@ def main():
     stats.add_number(1)
     stats.add_number(2)
     print("Numbers added: ", stats.count_numbers())
+    print("Sum of numbers: ", stats.get_sum())
+    print("Mean of numbers: ", stats.average())
 
 main()
