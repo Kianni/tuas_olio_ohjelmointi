37e5e2a95da024e03654e666e7149e141812369b
Kirill <kirill.nikolaev@everon.fi>
2024-02-09 19:45:40 +0200

    2.2 done

---
+++ exercise_4/task2/suitecase.py
@@ -0,0 +1,39 @@
+# File: serviceCharge.py
+# Author: Kirill Nikolaev
+# Description: Suitecase
+
+from item import Item
+
+class Suitcase:
+    def __init__(self, max_weight):
+        self._items = []
+        self._max_weight = max_weight
+
+    def add_item(self, item):
+        if item.weight() + sum(i.weight() for i in self._items) <= self._max_weight:
+            self._items.append(item)
+
+    def __str__(self):
+        total_weight = sum(item.weight() for item in self._items)
+        return f"{len(self._items)} items ({total_weight}g)"
+    
+def main():
+    book = Item("ABC Book", 200)
+    phone = Item("Nokia 3210", 100)
+    brick = Item("Brick", 400)
+
+    suitcase = Suitcase(500)
+
+    print(suitcase)
+
+    suitcase.add_item(book)
+    print(suitcase)
+
+    suitcase.add_item(phone)
+    print(suitcase)
+
+    suitcase.add_item(brick)
+    print(suitcase)
+
+if __name__ == "__main__":
+    main()
