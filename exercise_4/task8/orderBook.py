b6823ff38d51cea5033ae73d2bbc5cb9155da82e
Kirill <kirill.nikolaev@everon.fi>
2024-02-15 22:33:19 +0200

    task 8 part 4 done

--- exercise_4/task8/orderBook.py
+++ exercise_4/task8/orderBook.py
@@ -30,14 +30,27 @@ class OrderBook:
     def unfinished_orders(self):
         return [order for order in self.orders if not order.is_finished()]
     
+    def status_of_programmer(self, programmer):
+        finished = [order for order in self.orders if order.programmer == programmer and order.is_finished()]
+        unfinished = [order for order in self.orders if order.programmer == programmer and not order.is_finished()]
+
+        finished_hours = sum(order.workload for order in finished)
+        unfinished_hours = sum(order.workload for order in unfinished)
+
+        return len(finished), finished_hours, len(unfinished), unfinished_hours
+    
 def main():
     orders=OrderBook()
     orders.add_order("program webstore", "Adele", 10)
     orders.add_order("program mobile app for workload accounting", "Eric", 25)
     orders.add_order("program app for practising mathematics", "Adele", 100)
+    orders.add_order("program the next facebook","Eric",1000)
 
     orders.mark_finished(1)
     orders.mark_finished(2)
+
+    status=orders.status_of_programmer("Adele")
+    print(status)
 
     for order in orders.all_orders():
         print(order)
