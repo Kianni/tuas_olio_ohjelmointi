6c0f647cb8fa6429821c340f03c763bb569d8909
Kirill <kirill.nikolaev@everon.fi>
2024-02-15 20:28:32 +0200

    task 7 part 1 done

---
+++ exercise_4/task7/person.py
@@ -0,0 +1,37 @@
+# File: person.py
+# Author: Kirill Nikolaev
+# Description: Person class
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+        self._numbers = []
+        self._address = None
+
+    def name(self):
+        return self._name
+
+    def numbers(self):
+        return self._numbers
+
+    def address(self):
+        return self._address
+
+    def add_number(self, number):
+        self._numbers.append(number)
+
+    def add_address(self, address):
+        self._address = address
+
+def main():
+    person=Person("Eric")
+    print(person.name())
+    print(person.numbers())
+    print(person.address())
+    person.add_number("040-123456")
+    person.add_address("Mannerheimintie 10 Helsinki")
+    print(person.numbers())
+    print(person.address())
+
+if __name__ == "__main__":
+    main()
